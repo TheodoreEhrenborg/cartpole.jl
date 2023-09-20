306a010e27349d145584710e20dcd74629516587
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2023-09-20 23:18:22 +0100

    `nix run` will start up Pluto

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Cartpole in Julia";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      packages.default = pkgs.writeScriptBin "run_pluto" ''
+        ${pkgs.julia}/bin/julia -E "import Pkg; Pkg.add(\"Pluto\"); using Pluto; Pluto.run()"
+      '';
+      formatter = pkgs.alejandra;
+    });
+}
